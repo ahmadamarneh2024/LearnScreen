b7b1b8c50b5c50f635e6b0052fe82acf5f9bbdaf
Ahmad <ahmadamarna16@gmail.com>
2024-01-24 02:06:43 +0200

    layout issues

--- lib/main.dart
+++ lib/main.dart
@@ -1,226 +1,192 @@
 import 'package:flutter/material.dart';
-import 'BottomSheet.dart';
-
+import 'package:collection/collection.dart';
 
 void main() {
-  runApp(const Learn());
+  runApp(MyApp());
 }
 
-class Learn extends StatelessWidget {
-  const Learn({Key? key}) : super(key: key);
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text(
-            "Learn",
-            style: TextStyle(color: Color.fromRGBO(44, 63, 124, 1)),
-          ),
-          centerTitle: true,
-          backgroundColor: Colors.white,
-          elevation: 0.0,
-          leading: IconButton(
-            icon: Icon(Icons.exit_to_app_outlined, color: Colors.grey),
-            onPressed: () {
-
-            },
-          ),
-          actions: [
-            IconButton(
-              icon: Icon(Icons.more_vert, color: Colors.grey),
-              onPressed: () {
-                // Add the code to show the menu here
-                print('Three dots menu pressed');
-              },
-            ),
-          ],
+          title: Text('True/False Example'),
         ),
-        body: Column(
-          children: [
-            Container(
-              width: MediaQuery.of(context).size.width * 0.9,
-              child: const Divider(
-                color: Color.fromRGBO(169, 169, 169, 0.6),
-                height: 0.5,
-                thickness: 1.0,
-              ),
+        body: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Center(
+            child: MultipleChoiceOptions(
+              options: [
+                Option('True', true, 1),
+                Option('False', false, 2),
+                Option('False', false, 3),
+                Option('False', false, 4),
+              ],
             ),
-            Expanded(
-              child: LearnScreen(),
-            ),
-          ],
+          ),
         ),
       ),
     );
   }
 }
 
-class LearnScreen extends StatelessWidget {
-  const LearnScreen({Key? key}) : super(key: key);
+class MultipleChoiceOptions extends StatefulWidget {
+  final List<Option> options;
+
+  const MultipleChoiceOptions({
+    Key? key,
+    required this.options,
+  }) : super(key: key);
+
+  @override
+  _MultipleChoiceOptionsState createState() => _MultipleChoiceOptionsState();
+}
+
+class _MultipleChoiceOptionsState extends State<MultipleChoiceOptions> {
+  Option? selectedOption;
+  bool showCorrectAnswer = false;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            SizedBox(width: 10, height: 10),
-            RoundedCardWithShadow(),
-            SizedBox(height: 5),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: SizedBox(
-                    width: 70,
-                    height: 70,
-                    child: Image.asset(
-                      'lib/assest/goldcup.PNG',
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                ),
-                Text(
-                  "You've got this!",
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
-                ),
-              ],
-            ),
-            Card(
-              shape: RoundedRectangleBorder(
-                side: BorderSide(color: Colors.grey),
-                borderRadius: BorderRadius.circular(10),
-              ),
-              elevation: 2,
-              child: Padding(
-                padding: EdgeInsets.all(10),
-                child: Text(
-                  "10/60 Questions learnt ",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                      color: Color.fromRGBO(72, 72, 72, 1),
-                      fontWeight: FontWeight.bold,
-                      fontSize: 15),
-                ),
-              ),
-            ),
-            SizedBox(height: 10),
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(15, 0, 0, 0),
-                  child: Text(
-                    "Learnt in this round: ",
-                    style: TextStyle(fontSize: 15),
-                  ),
-                )
-              ],
-            ),
-            ElevatedButton(
-              onPressed: () {
-                _showBottomSheet(context);
-              },
-              child: Text("Show Bottom Sheet"),
-            ),
-          ],
+    return Column(
+      children: [
+        ...widget.options.map((option) => _buildOption(option)).toList(),
+        ElevatedButton(
+          onPressed: _submitAnswer,
+          child: Text('Submit'),
         ),
-      ),
+        SizedBox(height: 16.0),
+        ElevatedButton(
+          onPressed: _newButtonAction,
+          child: Text('New Button'),
+        ),
+        SizedBox(height: 16.0),
+        ElevatedButton(
+          onPressed: _clearSelections,
+          child: Text('Clear Selections'),
+        ),
+      ],
     );
   }
-}
-void _showBottomSheet(BuildContext context) {
-  showModalBottomSheet(
-    context: context,
-  //  isScrollControlled: true,
-    builder: (BuildContext context) {
-      return ShowBottomPage();
-    },
-  );
-}
 
+  Widget _buildOption(Option option) {
+    bool isCorrect = option.isCorrect && showCorrectAnswer;
+    bool isSelected = selectedOption == option;
+    bool isFalseSelected = isSelected && !option.isCorrect && showCorrectAnswer;
 
-
-
-class RoundedCardWithShadow extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      elevation: 4,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(10.0),
+    return Container(
+      margin: EdgeInsets.only(bottom: 16.0),
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: isCorrect
+              ? Color(0xff4caf50)
+              : isFalseSelected
+              ? Colors.red
+              : isSelected
+              ? Color(0xff234acd)
+              : Color(0xffc1c2c2),
+          width: 1,
+        ),
+        borderRadius: BorderRadius.circular(7.0),
       ),
-      child: IntrinsicHeight(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 14.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Icon(
-                    Icons.access_time_filled_sharp,
-                    color: Colors.blue,
-                  ),
-                  SizedBox(width: 8),
-                  Text(
-                    'Total Review Time: ',
-                    style: TextStyle(
-                      color: Colors.black,
-                    ),
-                  ),
-                  Text(
-                    '00:00:00',
-                    style: TextStyle(
-                      color: Color(0xFF3E62D4),
-                    ),
-                  ),
-                ],
-              ),
-              Container(
-                margin: EdgeInsets.symmetric(vertical: 8 ,),
-                width: MediaQuery.of(context).size.width * 0.9,
-                child: const Divider(
-                  color: Color.fromRGBO(
-                    169,
-                    169,
-                    169,
-                    0.6,
-                  ),
-                  height: 2,
-                  thickness: 0.5,
-                ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Icon(Icons.cloud_done_rounded, color: Colors.green),
-                  SizedBox(width: 8),
-                  Text(
-                    'Reviewed Terms: ',
-                    style: TextStyle(
-                      color: Colors.black,
-                    ),
-                  ),
-                  Text(
-                    '10/100',
-                    style: TextStyle(
-                      color: Color(0xFF3E62D4),
-                    ),
-                  ),
-                ],
-              ),
-            ],
+      child: ListTile(
+        tileColor: isCorrect
+            ? Color(0xffecf6ed)
+            : isFalseSelected
+            ? Color(0xfffbdcd6)
+            : isSelected
+            ? Color(0xfffffff)
+            : null,
+        title: Text(
+          option.text,
+          style: TextStyle(
+            color: isCorrect
+                ? Color(0xff4caf50)
+                : isFalseSelected
+                ? Colors.red
+                : isSelected
+                ? Color(0xff234acd)
+                : null,
           ),
         ),
+        leading: Stack(
+          children: [
+            Radio<Option>(
+              value: option,
+              groupValue: selectedOption,
+              onChanged: (Option? value) {
+                setState(() {
+                  // Toggle selection if the user taps on the already selected option
+                  selectedOption = (value == selectedOption) ? null : value;
+                });
+              },
+              activeColor: Colors.transparent,
+            ),
+            if (isSelected || isCorrect)
+              Positioned.fill(
+                child: Align(
+                  alignment: Alignment.center,
+                  child: Container(
+                    width: 18.0,
+                    height: 20.0,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: isCorrect
+                          ? Color(0xff4caf50)
+                          : isFalseSelected
+                          ? Colors.red
+                          : isSelected
+                          ? Color(0xff234acd)
+                          : Color(0xff234acd),
+                    ),
+                    child: Center(
+                      child: Icon(
+                        Icons.check,
+                        color: Colors.white,
+                        size: 16.0,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+          ],
+        ),
+        onTap: () {
+          setState(() {
+            // Toggle selection when the user taps on an option
+            selectedOption = (selectedOption == option) ? null : option;
+          });
+        },
       ),
     );
   }
+
+  void _submitAnswer() {
+    setState(() {
+      // Always show the correct answer after submitting
+      showCorrectAnswer = true;
+    });
+  }
+
+  void _newButtonAction() {
+    // Implement the action for your new button here
+    print('New Button Pressed');
+  }
+
+  void _clearSelections() {
+    setState(() {
+      // Clear selected options and reset colors
+      selectedOption = null;
+      showCorrectAnswer = false;
+    });
+  }
+}
+
+class Option {
+  final String text;
+  final bool isCorrect;
+  final int id;
+
+  Option(this.text, this.isCorrect, this.id);
 }
